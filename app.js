0c8e30d93cf9444f503086755024b3c12f4e73b1
Maxwell Corbin <maxwell.corbin@gmail.com>
2016-06-04 18:43:06 -0400

    basic postmate 2

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var Postmates = require('postmates');
+var postmates = new Postmates('cus_Kp8cLL8C8C0Sp-', '3fc8dbff-8687-4623-833b-75f0665eaaae');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,19 +34,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
 
 // production error handler
 // no stacktraces leaked to user
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var Postmates = require('postmates');
+var postmates = new Postmates('cus_Kp8cLL8C8C0Sp-', '3fc8dbff-8687-4623-833b-75f0665eaaae');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var delivery = {
+		pickup_address: "20 McAllister St, San Francisco, CA",
+		dropoff_address: "101 Market St, San Francisco, CA"
+  };
+
+	postmates.quote(delivery, function(err, response) {
+		res.render('index', { quote: response.body });
+	});
+
 });
 
 module.exports = router;
