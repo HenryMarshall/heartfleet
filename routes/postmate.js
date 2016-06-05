f3c307120bd1c3c04057530c6f355f438804c18e
Maxwell Corbin <maxwell.corbin@gmail.com>
2016-06-04 23:09:43 -0400

    postmate working

--- routes/postmate.js
+++ routes/postmate.js
@@ -1,22 +1,24 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
+// var bodyParser = require('body-parser');
+// router.use(bodyParser.json()); // support json encoded bodies
+// router.use(bodyParser.urlencoded({ extended: true }));
 
+// var Postmates = require('postmates');
+// var postmates = new Postmates('cus_Kp8cLL8C8C0Sp-', '3fc8dbff-8687-4623-833b-75f0665eaaae');
 
-var Postmates = require('postmates');
-var postmates = new Postmates('cus_Kp8cLL8C8C0Sp-', '3fc8dbff-8687-4623-833b-75f0665eaaae');
+// router.use(express.bodyParser());
 
-router.use(express.bodyParser());
+// router.post('/', function(req, res, next) {
+// 	var delivery = {
+// 		pickup_address: req.body.pickup_address,
+// 		dropoff_address: req.body.dropoff_address
+//   	};
 
-router.post('/', function(req, res, next) {
-	var delivery = {
-		pickup_address: req.body.pickup_address,
-		dropoff_address: req.body.dropoff_address
-  	};
+// 	postmates.quote(delivery, function(err, response) {
+// 		res.send({ quote: response.body });
+// 	});
 
-	postmates.quote(delivery, function(err, response) {
-		res.send({ quote: response.body });
-	});
+// });
 
-});
-
-module.exports = router;
+// module.exports = router;
